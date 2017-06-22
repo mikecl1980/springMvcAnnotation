345081a2a4f94e9c2981029489b8a0d1c7ae9f43
Administrator <Administrator@HY50-20161009RV>
2017-06-22 16:11:52 +0800

    mybatis

--- SpringMvcAnnotation/src/mikecl/example/WebConfig.java
+++ SpringMvcAnnotation/src/mikecl/example/WebConfig.java
@@ -1,25 +1,32 @@
 package mikecl.example;
 
+
+
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.ContextLoader;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("mikecl.example")
+@MapperScan("mikecl.example.mapper")
 public class WebConfig extends WebMvcConfigurerAdapter 
 {
 	@Bean
@@ -49,4 +56,32 @@ public class WebConfig extends WebMvcConfigurerAdapter
 		resolver.setContentType("text/html;charset=UTF-8");
 		return resolver;
 	}
+	
+	@Bean  
+	public DataSource dataSource()
+	{  
+		BasicDataSource dataSource = new BasicDataSource();	
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");  
+		dataSource.setUrl("jdbc:mysql://localhost:3306/mvc?useUnicode=true&amp;characterEncoding=utf8");  
+		dataSource.setUsername("root");  
+		dataSource.setPassword("123456"); 
+        return dataSource;  
+    }  
+	
+	@Bean  
+	public SqlSessionFactoryBean sqlSessionFactory()
+	{  
+	    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();  
+	    sqlSessionFactoryBean.setDataSource(this.dataSource());
+	    sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("/mikecl/example/entity/xml/MyBatisConfig.xml"));  
+	    return sqlSessionFactoryBean;  
+	} 
+	
+	@Bean  
+	public SqlSessionTemplate sqlSessionTemplate() throws Exception
+	{  
+	    SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(this.sqlSessionFactory().getObject());  
+	    return sqlSessionTemplate;  
+	}
+	
 }
--- SpringMvcAnnotation/src/mikecl/example/controller/IndexController.java
+++ SpringMvcAnnotation/src/mikecl/example/controller/IndexController.java
@@ -1,16 +1,26 @@
 package mikecl.example.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import mikecl.example.entity.User;
+import mikecl.example.service.UserService;
 
 @Controller
-public class IndexController {
-
+public class IndexController 
+{
+	@Resource
+	private UserService userService;
+	
 	@GetMapping("/**")
 	public String index()
 	{
-		System.out.println(11);
+		User u = userService.findById(Long.valueOf("1"));
+		System.out.println(u.getName());
+		System.out.println(u.getUserid());
+		System.out.println(1);
 		return "index";
 	}
 }
