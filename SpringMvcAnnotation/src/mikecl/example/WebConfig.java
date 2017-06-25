01d0ab635b945dca0fd1aaba0b1f2bbae55dacd1
ZJU <ZJU@ZJU-PC>
2017-06-25 14:36:18 +0800

    a

--- SpringMvcAnnotation/src/mikecl/example/WebConfig.java
+++ SpringMvcAnnotation/src/mikecl/example/WebConfig.java
@@ -2,18 +2,23 @@ package mikecl.example;
 
 
 
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -24,7 +29,10 @@ import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
+import com.alibaba.druid.pool.DruidDataSource;
+
 @Configuration
+@PropertySource(value="classpath:application.properties") 
 @EnableWebMvc
 @ComponentScan("mikecl.example")
 @MapperScan("mikecl.example.mapper")
@@ -59,31 +67,46 @@ public class WebConfig extends WebMvcConfigurerAdapter
 	}
 	
 	@Bean  
-	public DataSource dataSource()
+	public DataSource dataSource() 
 	{  
-		BasicDataSource dataSource = new BasicDataSource();	
+		DruidDataSource dataSource = new DruidDataSource();	
+		Resource resource = new ClassPathResource("application.properties");
+		Properties props;
+		try {
+			props = PropertiesLoaderUtils.loadProperties(resource);
+			dataSource.setDriverClassName(props.getProperty("spring.datasource.driverClassName"));  
+			dataSource.setUrl(props.getProperty("spring.datasource.url"));  
+			dataSource.setUsername(props.getProperty("spring.datasource.username"));  
+			dataSource.setPassword(props.getProperty("spring.datasource.password"));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	/*	
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");  
-		dataSource.setUrl("jdbc:mysql://localhost:3306/mvc?useUnicode=true&amp;characterEncoding=utf8");  
+		dataSource.setUrl("jdbc:mysql://118.178.129.111:3306/test?useUnicode=true&amp;characterEncoding=utf8");  
 		dataSource.setUsername("root");  
-		dataSource.setPassword("123456"); 
+		dataSource.setPassword("MyNewPass4!"); */
         return dataSource;  
-    }  
+    } 
 	
 	@Bean  
 	@Autowired
-	public SqlSessionFactoryBean sqlSessionFactory()
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource)
 	{  
 	    SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();  
-	    sqlSessionFactoryBean.setDataSource(this.dataSource());
+	    //sqlSessionFactoryBean.setDataSource(this.dataSource());
+	    sqlSessionFactoryBean.setDataSource(dataSource);
 	    sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("/mikecl/example/entity/xml/MyBatisConfig.xml"));  
 	    return sqlSessionFactoryBean;  
 	} 
 	
-	@Bean  
+	/*@Bean  
 	public SqlSessionTemplate sqlSessionTemplate() throws Exception
 	{  
 	    SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(this.sqlSessionFactory().getObject());  
 	    return sqlSessionTemplate;  
-	}
+	}*/
 	
 }
